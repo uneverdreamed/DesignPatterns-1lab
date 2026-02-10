94846ac9360a406fab70de7f5e3993430355811f
uneverdreamed <ma2005f@gmail.com>
2026-02-10 18:53:45 +0300

    создан класс TelemetryPacket

---
+++ TelemetryCore/models/TelemetryPacket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelemetryCore.models
+{
+    /// <summary>
+    /// пакет телеметрических данных
+    /// </summary>
+    public class TelemetryPacket
+    {
+        /// <summary>
+        /// временная метка получения данных
+        /// </summary>
+        public DateTime Timestamp { get; set; }
+        /// <summary>
+        /// сырые данные от датчика
+        /// </summary>
+        public byte[] RawData { get; set; }
+
+        /// <summary>
+        /// тип сенсора от которого получены данные
+        /// </summary>
+        public string SensorType { get; set; }
+
+        public TelemetryPacket(byte[] rawData, string sensorType)
+        {
+            Timestamp = DateTime.Now;
+            RawData = rawData;
+            SensorType = sensorType;
+        }
+
+    }
+}
