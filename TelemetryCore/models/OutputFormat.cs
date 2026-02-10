ad354d6e5fad47e295c727d000fb913284a8531c
uneverdreamed <ma2005f@gmail.com>
2026-02-10 18:37:22 +0300

    создан enum OutputFormat

---
+++ TelemetryCore/models/OutputFormat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelemetryCore.models
+{
+    public enum OutputFormat
+    {
+        /// <summary>
+        /// формат JSON
+        /// </summary>
+        JSON,
+        /// <summary>
+        /// формат XML
+        /// </summary>
+        XML,
+        /// <summary>
+        /// простой тектовый формат
+        /// </summary>
+        PlainText
+    }
+}
