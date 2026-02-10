f956b8af1e19ed0f17edb3cf5f2e39c066cbcffc
uneverdreamed <ma2005f@gmail.com>
2026-02-10 19:10:49 +0300

    создан интерфейс IAnalyzerBuilder

---
+++ TelemetryCore/interfaces/IAnalyzerBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelemetryCore.models;
+
+namespace TelemetryCore.interfaces
+{
+    /// <summary>
+    /// Интерфейс строителя анализатора телеметрии. Реализует паттерн "Строитель"
+    /// </summary>
+    public interface IAnalyzerBuilder
+    {
+        /// <summary>
+        /// установить данные калибровки
+        /// </summary>
+        /// <param name="data"></param>
+        void SetCalibration(CalibrationData data);
+
+        /// <summary>
+        /// установить конфигурацию фильтров
+        /// </summary>
+        /// <param name="filters"></param>
+        void SetFilters(FilterConfig filters);
+
+        /// <summary>
+        /// установить формат вывода отчета
+        /// </summary>
+        /// <param name="format"></param>
+        void SetOutputFormat(OutputFormat format);
+
+        /// <summary>
+        /// построить и вернуть настроенный анализатор
+        /// </summary>
+        /// <returns>готовый к работе анализатор</returns>
+        ITelemetryAnalyzer Build();
+
+        /// <summary>
+        /// сбросить строитель для создания нового анализатора
+        /// </summary>
+        void Reset();
+    }
+}
