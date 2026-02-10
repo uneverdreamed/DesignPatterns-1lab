beafb468abf0e608c2268eb44bd17fb9b72320d5
uneverdreamed <ma2005f@gmail.com>
2026-02-10 17:50:46 +0300

    создан интерфейс ITelemetryAnalyzer

---
+++ TelemetryCore/interfaces/ITelemetryAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelemetryCore.interfaces
+{
+    /// <summary>
+    /// Интерфейс анализатора телеметрии
+    /// </summary>
+    public interface ITelemetryAnalyzer
+    {
+        /// <summary>
+        /// Инициализация анализатора
+        /// </summary>
+        void Initialize();
+        /// <summary>
+        /// Обработка сырых данных телеметрии
+        /// </summary>
+        /// <param name="rawData"></param>
+        void ProcessData(byte[] rawData);
+        /// <summary>
+        /// Получение отчета о результатах анализа
+        /// </summary>
+        /// <returns></returns>
+        string GetSupport();
+        /// <summary>
+        /// Завершение работы анализатора и освобождение ресурсов
+        /// </summary>
+        void Terminate();
+    }
+}
