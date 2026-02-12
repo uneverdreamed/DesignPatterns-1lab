e808e989625156285264897248920ab0b26ca69f
uneverdreamed <ma2005f@gmail.com>
2026-02-12 17:18:25 +0300

    реализован RadarFactory

---
+++ RadarPlugin/RadarFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelemetryCore.interfaces;
+using TelemetryCore.models;
+
+namespace RadarPlugin
+{
+    public class RadarFactory : AnalyzerFactory
+    {
+        public override ITelemetryAnalyzer CreateAnalyzer()
+        {
+            return new RadarAnalyzer();
+        }
+
+        public override string GetPluginName() => "Radar Analyzer";
+        public override string GetPluginVersion() => "1.0.0";
+    }
+}
+ 
