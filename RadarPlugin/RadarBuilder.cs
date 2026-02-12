182e9aac0de0d3be5fca9474b674f95e0257c19f
uneverdreamed <ma2005f@gmail.com>
2026-02-12 17:24:15 +0300

    реализован RadarBuilder

---
+++ RadarPlugin/RadarBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelemetryCore.interfaces;
+using TelemetryCore.models;
+
+namespace RadarPlugin
+{
+        public class RadarBuilder : IAnalyzerBuilder
+        {
+            private RadarAnalyzer _analyzer = new();
+
+            public void SetCalibration(CalibrationData data)
+            {
+                _analyzer.Calibration = data;
+            }
+
+            public void SetFilters(FilterConfig filters)
+            {
+                _analyzer.Filters = filters;
+            }
+
+            public void SetOutputFormat(OutputFormat format)
+            {
+                _analyzer.OutputFormat = format;
+            }
+
+            public ITelemetryAnalyzer Build()
+            {
+                var result = _analyzer;
+                Reset();
+                return result;
+            }
+
+            public void Reset()
+            {
+                _analyzer = new RadarAnalyzer();
+            }
+        }
+    
+}
